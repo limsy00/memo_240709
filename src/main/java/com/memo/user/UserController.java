936c4d8ffcfa0b86f1be52ed89c3978962b90fc4
DESKTOP-L7G5VSR\user <test@test.com>
2024-07-11 19:01:11 +0900

    240711 레이아웃 및 회원가입/ 로그인 마크업 정돈

---
+++ src/main/java/com/memo/user/UserController.java
@@ -0,0 +1,16 @@
+package com.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	@GetMapping("/sign-up-view")
+	public String signUpView() {
+		// 가운데 레이아웃 조각만 내려주면 전체 레이아웃으로 구성된다.
+		return "user/signUp";
+	}
+}
